78f2bdcb4bc91dc6f7f242ad6e37464058dd98b9
yesg <46396439+yesgvinayak@users.noreply.github.com>
2021-10-21 22:47:21 +0530

    testing-1

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 8080
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run')
+    res.end('Hello Randomtrees')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
